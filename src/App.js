72981e914e978437f11fd4a6fd92c807aadc7ac4
Shivam Sharma <shivam691999@gmail.com>
2021-02-13 16:39:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, Fragment } from "react";
+import Tabletop from "tabletop";
+// import "./styles.css";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: "1pat2DELMEuTPEgHKbSDXK6Ci5HKJpqWsbOTdkUAueeE",
+      simpleSheet: true
+    })
+      .then((data) => setData(data))
+      .catch((err) => console.warn(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <br /><br /><br />
+      <h1>All Event Details: </h1>
+      <br /><br />
+
+      <table style={{borderStyle:'groove', borderCollapse:'collapse'}}>
+        <tr >
+          <td style={{borderStyle:'groove'}}><b>Event Name</b></td>
+          <td style={{borderStyle:'groove'}}><b>Event Place</b></td>
+          <td style={{borderStyle:'groove'}}><b>Event Start Time</b></td>
+          <td style={{borderStyle:'groove'}}><b>Event End Time</b></td>
+        </tr>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td style={{border:'1px solid black'}}>{item.EventName}</td>
+            <td style={{border:'1px solid black'}}>{item.EventPlace}</td>
+            <td style={{border:'1px solid black'}}>{item.EventStartTime}</td>
+            <td style={{border:'1px solid black'}}>{item.EventEndTime}</td>
+            
+            <br />
+          </tr>
+        ))}
+      </table>
+    </center>
   );
 }
-
-export default App;
